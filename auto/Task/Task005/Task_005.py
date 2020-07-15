29ae795d505c689e0983aa48aeee2ac20fcc45f2
1037712339@qq.com <q@@13797274963>
2020-07-15 22:44:23 +0800

    作业

---
+++ auto/Task/Task005/Task_005.py
@@ -0,0 +1,13 @@
+# author：wuziwei   
+# @time:2020-07-15 22:25
+# coding:-*- utf-8 -*-
+'''
+http://vip.ytesting.com/q.do?a&id=ff80808172480d2501724baa8a7801ca
+'''
+
+from selenium import webdriver
+
+
+class Login:
+    def __init__(self):
+        pass
